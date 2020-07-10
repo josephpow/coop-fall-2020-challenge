f7ac10dc3918929414f7f18fd7493528d9972337
josephpow <59949913+josephpow@users.noreply.github.com>
2020-07-10 13:29:49 -0400

    Add files via upload

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,40 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.eventStack = []
+        self.redoStack = []
 
     def add(self, num: int):
+        self.eventStack.append(num)
+        self.value += num
         pass
 
     def subtract(self, num: int):
+        self.eventStack.append(-num)
+        self.value -= num
         pass
 
     def undo(self):
+        if len(self.eventStack) > 0:
+            event = self.eventStack.pop()
+            self.value -= event
+            self.redoStack.append(event)
         pass
 
     def redo(self):
+        if len(self.redoStack) > 0:
+            event = self.redoStack.pop()
+            self.value += event
         pass
 
     def bulk_undo(self, steps: int):
+        while steps > 0:
+            self.undo()
+            steps -= 1
         pass
 
     def bulk_redo(self, steps: int):
+        while steps > 0:
+            self.redo()
+            steps -= 1
         pass
