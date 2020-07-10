1e556b66992ecac6031900ca8ca9a437ad5b4d87
josephpow <59949913+josephpow@users.noreply.github.com>
2020-07-10 14:04:08 -0400

    Cleaned up solution, added testing for edge case
    Tags: Fall 2020 Co-op, Student

--- solution_python.py
+++ solution_python.py
@@ -1,5 +1,5 @@
 class EventSourcer():
-    # Do not change the signature of any functions
+    # Do not change the signature of any function
 
     def __init__(self):
         self.value = 0
@@ -9,34 +9,27 @@ class EventSourcer():
     def add(self, num: int):
         self.eventStack.append(num)
         self.value += num
-        pass
 
     def subtract(self, num: int):
         self.eventStack.append(-num)
         self.value -= num
-        pass
 
     def undo(self):
         if len(self.eventStack) > 0:
             event = self.eventStack.pop()
             self.value -= event
             self.redoStack.append(event)
-        pass
 
     def redo(self):
         if len(self.redoStack) > 0:
             event = self.redoStack.pop()
+            self.eventStack.append(event)
             self.value += event
-        pass
 
     def bulk_undo(self, steps: int):
-        while steps > 0:
+        for _ in range(min(len(self.eventStack), steps)):
             self.undo()
-            steps -= 1
-        pass
 
     def bulk_redo(self, steps: int):
-        while steps > 0:
+        for _ in range(min(len(self.redoStack), steps)):
             self.redo()
-            steps -= 1
-        pass
